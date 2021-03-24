8b3dcf2e3b2aafd912bd63153fa9fdb7c2a911ff
enaee <enaevic@gmail.com>
2021-03-24 20:31:09 +0100

    Make .csv file if student is present

--- attendance.py
+++ attendance.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import face_recognition
 import os
+from datetime import datetime
 
 
 def find_encodings(imagesList):
@@ -11,6 +12,19 @@ def find_encodings(imagesList):
         encode = face_recognition.face_encodings(img)[0]
         encode_list.append(encode)
     return encode_list
+
+
+def mark_attendance(name):
+    with open('attendanceRecord.csv', 'r+') as f:
+        myDataList = f.readlines()
+        nameList = []
+        for line in myDataList:
+            entry = line.split(',')
+            nameList.append(entry[0])
+        if name not in nameList:
+            now = datetime.now()
+            dtString = now.strftime('%H:%M:%S')
+            f.writelines(f'\n{name},{dtString}')
 
 
 path = 'imagesDB'
@@ -31,7 +45,7 @@ cap = cv2.VideoCapture(0)
 
 while True:
     success, img = cap.read()
-    imgS = cv2.resize(img,(0,0), None, 0.25, 0.25)
+    imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
     imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
     facesInFrame = face_recognition.face_locations(imgS)
     encodesInFrame = face_recognition.face_encodings(imgS, facesInFrame)
@@ -41,13 +55,14 @@ while True:
         faceDistance = face_recognition.face_distance(encodeListKnow, encodeFace)
         matchIndex = np.argmin(faceDistance)
 
-        if matches [matchIndex]:
-            name = names [matchIndex].upper()
+        if matches[matchIndex]:
+            name = names[matchIndex].upper()
             y1, x2, y2, x1 = faceLoc
             y1, x2, y2, x1 = y1*4, x2*4, y2*4, x1*4
             cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
             cv2.rectangle(img, (x1, y2-35), (x2, y2), (0, 255, 0), cv2.FILLED)
             cv2.putText(img, name, (x1+6, y2-6), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+            mark_attendance(name)
 
     cv2.imshow('Webcam', img)
     cv2.waitKey(1)
